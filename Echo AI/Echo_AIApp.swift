625f74db9cc9d0bd720ae72e75b08ba959616d1c
ryalilakshman123 <lakshmanryali123@gmail.com>
2025-08-24 00:30:28 +0530

    Initial Commit

---
+++ Echo AI/Echo_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Echo_AIApp.swift
+//  Echo AI
+//
+//  Created by Sameer Nikhil on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Echo_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
